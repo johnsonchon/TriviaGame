435eb836f5e12b2fcf46018579c88cafb752dd50
johnsonchon <thejohnson.does.it@gmail.com>
2018-12-02 20:45:45 -0800

    not done

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    // variables
+
+
+
+
+    //Once Button is clicked
+
+    $(".btn").click(function () {
+
+        // The timer
+        
+
+        $(".lead").html("Time Remaining: ");
+        // Hides the button
+        $(".btn").hide();
+
+
+        //Questions
+        // Question 1
+        $(".q-one h2").html('"And You Will Know My Name Is The Lord When I Lay My Vengeance Upon Thee!"');
+
+        $(".one-1").html("Pulp Fiction");
+        $(".one-2").html("Star Wars");
+        $(".one-3").html("Shaft");
+        $(".one-4").html("Avengers");
+
+        // Question 2
+        $(".q-two h2").html('"My duty to please that booty"');
+
+        $(".two-1").html("Pulp Fiction");
+        $(".two-2").html("Star Wars");
+        $(".two-3").html("Shaft");
+        $(".two-4").html("Avengers");
+
+
+
+
+
+
+
+
+
+    });
+
+
+});
